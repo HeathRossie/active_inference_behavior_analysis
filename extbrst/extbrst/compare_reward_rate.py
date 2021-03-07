f62c29d49c7e7a70ad56d5fdea763f2a48c4841e
7cm-diameter <from.this.ridiculous.loop@gmail.com>
2021-03-07 16:19:53 +0900

    behavior under single schedule as a choice with other behaviors

--- extbrst/extbrst/compare_reward_rate.py
+++ extbrst/extbrst/compare_reward_rate.py
@@ -1,11 +1,9 @@
-from typing import Any, Iterable, List, Tuple
+from typing import List, Tuple
 
 import numpy as np
-from nptyping import NDArray
 
-from extbrst.model import (Action, Agent, GAIAgent, Prediction, Probability,
-                           Reward)
-from extbrst.schedule import (Extinction, Schedule, VariableInterval,
+from extbrst.model import Action, Agent, Prediction, Probability, Reward
+from extbrst.schedule import (ConcurrentSchedule, Extinction, VariableInterval,
                               VariableRatio)
 
 NumberOfTrial = int
@@ -13,39 +11,42 @@ Result = Tuple[Action, Reward, Prediction, Probability]
 OutputData = Tuple[Probability, Action, Reward, Prediction, Probability]
 
 
-class ConcurrentSchedule(object):
-    def __init__(self, schedules: List[Schedule]):
-        self.__schedules = schedules
+def run_baseline(agent: Agent, schedule: ConcurrentSchedule, timestep: float):
+    def trial_process(agent: Agent, schedule: ConcurrentSchedule,
+                      timestep: float) -> Result:
+        preds = agent.predict()
+        probs = agent.calculate_response_probs(-preds)
+        actions = agent.choose_action(probs)
+        counts = actions.copy().tolist()
+        counts[1] = timestep
+        rewards = schedule.step(counts, actions.tolist())
+        agent.update(rewards, actions.astype(np.uint8))
+        ret = actions[0], rewards[0], preds[0], probs[0]
+        return ret
 
-    def step(self, counts: Iterable[Any],
-             actions: Iterable[Action]) -> NDArray[1, Reward]:
-        rewards: List[int] = []
-        for s, c, a in zip(self.__schedules, counts, actions):
-            rewards.append(s.step(c, a))
-        return np.array(rewards)
-
-    def finished(self) -> bool:
-        return self.__schedules[0].finished()
-
-
-def trial_process(agent: Agent, schedule: ConcurrentSchedule,
-                  timestep: float) -> Result:
-    preds = agent.predict()
-    probs = agent.calculate_response_probs(-preds)
-    actions = agent.choose_action(probs)
-    counts = actions.copy().tolist()
-    counts[1] = timestep
-    rewards = schedule.step(counts, actions)
-    agent.update(rewards, actions.astype(np.uint8))
-    ret = actions[0], rewards[0], preds[0], probs[0]
-    return ret
-
-
-def run(agent: Agent, schedule: ConcurrentSchedule,
-        timestep: float) -> List[Result]:
     results: List[Result] = []
-    while not conc.finished():
-        results.append(trial_process(agent, conc, timestep))
+    while not schedule.finished():
+        results.append(trial_process(agent, schedule, timestep))
+    return results
+
+
+def run_extinction(agent: Agent, schedule: ConcurrentSchedule,
+                   timestep: float):
+    counts = [timestep for _ in range(len(schedule.val))]
+
+    def trial_process(agent: Agent, schedule: ConcurrentSchedule,
+                      timestep: float) -> Result:
+        preds = agent.predict()
+        probs = agent.calculate_response_probs(-preds)
+        actions = agent.choose_action(probs)
+        rewards = schedule.step(counts, actions.tolist())
+        agent.update(rewards, actions.astype(np.uint8))
+        ret = actions[0], rewards[0], preds[0], probs[0]
+        return ret
+
+    results: List[Result] = []
+    while not schedule.finished():
+        results.append(trial_process(agent, schedule, timestep))
     return results
 
 
@@ -55,28 +56,32 @@ if __name__ == '__main__':
     from extbrst.model import GAIAgent
     from extbrst.util import get_nth_ancestor
 
-    requirements: List[Probability] = [1.]
+    requirements: List[Probability] = [1 + 1e-8]
     extinction: Probability = 0.
-    baseline_lenght: NumberOfTrial = 200
-    extinction_lenght: NumberOfTrial = 1000
+    baseline_lenght: NumberOfTrial = 500
+    extinction_lenght: NumberOfTrial = 3600
+    num_alt = 10
 
     results: List[List[OutputData]] = []
-    # run simulations for each baseline reward probability
+    # run simulations for each VR schedule
     for rq in requirements:
-        agent = GAIAgent(0.5, 2)
-        vr = VariableRatio()
-        vr.config(rq, baseline_lenght, 0)
-        vi = VariableInterval()
-        vi.config(30., 1000, 1.)
-        conc = ConcurrentSchedule([vr, vi])
-        _baseline_result = run(agent, conc, 1.)
+        agent = GAIAgent(1., 1 + num_alt)  # operant + alternative behaviors
+        baseline_schedule = VariableRatio(rq, baseline_lenght, 0)
+        alternative_schedules = [
+            VariableInterval(120., 1000, 1.) for _ in range(num_alt)
+        ]
+
+        baseline_schedules = ConcurrentSchedule([baseline_schedule] +
+                                                alternative_schedules)
+        _baseline_result = run_baseline(agent, baseline_schedules, 1.)
+
+        ext = Extinction(extinction_lenght)
+        [alt.reset for alt in alternative_schedules]
+        extinction_schedules = ConcurrentSchedule([ext] +
+                                                  alternative_schedules)
+        _ext_result = run_extinction(agent, extinction_schedules, 1.)
         baseline_result: List[OutputData] = \
-            list(map(lambda br: (rq, ) + br, _baseline_result))
-        ext = Extinction()
-        ext.config(1., extinction_lenght, 0)
-        vi.config(30., 1000, 1.)
-        conc = ConcurrentSchedule([ext, vi])
-        _ext_result = run(agent, conc, 1.)
+            list(map(lambda br: (1 / rq, ) + br, _baseline_result))
         ext_result: List[OutputData] = \
             list(map(lambda er: (extinction, ) + er, _ext_result))
         results.append(baseline_result + ext_result)
