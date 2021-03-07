50720672d3c9dcf4f0e43c2a431bbb25c893f6f3
7cm-diameter <from.this.ridiculous.loop@gmail.com>
2021-03-07 16:18:57 +0900

    add `schedule.py`

--- extbrst/extbrst/schedule.py
+++ extbrst/extbrst/schedule.py
@@ -1,39 +1,69 @@
-from abc import ABCMeta, abstractmethod
-from typing import Any
+from abc import ABCMeta, abstractmethod, abstractproperty
+from typing import Any, Iterable, List, Sequence, Union
 
 import numpy as np
 import scipy.stats as st
+from nptyping import NDArray
 
 from extbrst.util import randomize
 
 
+def _exp_rng(mean: float, n: int, _min: float) -> NDArray[1, float]:
+    return st.expon.ppf(np.linspace(0.01, 0.99, n), scale=mean, loc=_min)
+
+
+def _geom_rng(mean: float, n: int, _min: float) -> NDArray[1, float]:
+    return st.geom.ppf(np.linspace(0.01, 0.99, n),
+                       p=1 / (mean - _min),
+                       loc=_min)
+
+
 class Schedule(metaclass=ABCMeta):
     @abstractmethod
-    def step(self, count: Any, action: int) -> Any:
+    def step(self, count: Union[Any, Iterable[Any]],
+             action: Union[int, Iterable[int]]) -> Union[int, Iterable[int]]:
         pass
 
     @abstractmethod
-    def config(self, val: float, n: int, _min: float = 0.) -> int:
+    def config(self, val: float, n: int, _min: float):
         pass
 
     @abstractmethod
     def finished(self) -> bool:
         pass
 
+    @abstractmethod
+    def reset(self):
+        pass
+
+    @abstractproperty
+    def val(self) -> Any:
+        pass
+
+    @abstractproperty
+    def n(self) -> Any:
+        pass
+
+    @abstractproperty
+    def vals(self) -> Any:
+        pass
+
 
 class VariableInterval(Schedule):
-    def __init__(self):
-        self.__mean_intervals = None
+    def __init__(self, val: float, n: int, _min: float):
+        self.__val = val
+        self.__min = _min
+        self.__n = n
         self.__count = 0
+        self.__intervals = randomize(_exp_rng(val, n, _min))
+        self.__interval = self.__intervals[self.__count]
 
     def config(self, val: float, n: int, _min: float):
         self.__val = val
         self.__min = _min
         self.__n = n
-        intervals = st.expon.ppf(np.linspace(0.01, 0.99, n),
-                                 scale=val,
-                                 loc=_min)
-        self.__intervals = randomize(intervals)
+        self.__count = 0
+        self.__intervals = randomize(_exp_rng(val, n, _min))
         self.__interval = self.__intervals[self.__count]
 
     def step(self, count: float, action: int) -> int:
@@ -42,80 +72,140 @@ class VariableInterval(Schedule):
             self.__count += 1
             if not self.finished():
                 self.__interval = self.__intervals[self.__count]
-                print(self.__interval)
             return 1
         return 0
 
     def finished(self) -> bool:
         return self.__count >= self.__n
 
-
-class FixedRatio(Schedule):
-    def __init__(self):
-        self.__required_responses = None
+    def reset(self):
         self.__count = 0
+        self.__intervals = randomize(self.__intervals)
+        self.__interval = self.__intervals[self.__count]
 
-    def config(self, val: float, n: int, _min: int):
-        self.__val = val
-        self.__min = _min
-        self.__n = n
-        self.__required_responses = np.array([val for _ in range(n)])
-        self.__required_response = self.__required_responses[self.__count]
+    @property
+    def val(self) -> float:
+        return self.__val
 
-    def step(self, count: int, action: int) -> int:
-        self.__required_response -= count
-        if action == 1:
-            self.__count += 1
-            if not self.finished:
-                self.__required_response = self.__required_responses[
-                    self.__count]
-            return 1
-        return 0
+    @property
+    def n(self) -> int:
+        return self.__n
 
-    def finished(self) -> bool:
-        return self.__count >= self.__n
+    @property
+    def vals(self) -> NDArray[1, float]:
+        return self.__intervals
 
 
 class VariableRatio(Schedule):
-    def __init__(self):
-        self.__required_responses = None
-        self.__count = 0
-
-    def config(self, val: float, n: int, _min: int):
+    def __init__(self, val: float, n: int, _min: float):
         self.__val = val
         self.__min = _min
         self.__n = n
-        required_responses = st.geom.ppf(np.linspace(0.01, 0.99, n),
-                                         p=1 / (val - _min),
-                                         loc=_min)
-        self.__required_responses = randomize(required_responses)
-        self.__required_response = self.__required_responses[self.__count]
+        self.__count = 0
+        self.__responses = randomize(_geom_rng(val, n, _min))
+        self.__response = self.__responses[self.__count]
+
+    def config(self, val: float, n: int, _min: float):
+        self.__val = val
+        self.__min = _min
+        self.__n = n
+        self.__count = 0
+        self.__responses = randomize(_geom_rng(val, n, _min))
+        self.__response = self.__responses[self.__count]
 
     def step(self, count: int, action: int) -> int:
-        self.__required_response -= count
-        if action == 1 and self.__required_response <= 0:
+        self.__response -= count
+        if self.__response <= 0 and action == 1:
             self.__count += 1
             if not self.finished():
-                self.__required_response = self.__required_responses[
-                    self.__count]
+                self.__response = self.__responses[self.__count]
             return 1
         return 0
 
     def finished(self) -> bool:
         return self.__count >= self.__n
 
+    def reset(self):
+        self.__count = 0
+        self.__responses = randomize(self.__responses)
+        self.__response = self.__responses[self.__count]
+
+    @property
+    def val(self) -> float:
+        return self.__val
+
+    @property
+    def n(self) -> int:
+        return self.__n
+
+    @property
+    def vals(self) -> NDArray[1, float]:
+        return self.__responses
+
 
 class Extinction(Schedule):
-    def __init__(self):
+    def __init__(self, val: float):
+        self.__val = val
         self.__count = 0
 
-    def config(self, val: float, n: int, _min: int):
-        self.__n = n
-        pass
+    def config(self, val: float, n: int = 0, _min: float = 0):
+        _, _ = n, _min
+        self.__val = val
 
-    def step(self, count: int, action: int) -> int:
-        self.__count += 1
+    def step(self, count: float, action: int) -> int:
+        _ = action
+        self.__count += count
         return 0
 
     def finished(self) -> bool:
-        return self.__count >= self.__n
+        return self.__count >= self.__val
+
+    def reset(self):
+        self.__count = 0
+
+    @property
+    def val(self) -> float:
+        return self.__val
+
+    @property
+    def n(self) -> None:
+        pass
+
+    @property
+    def vals(self) -> None:
+        pass
+
+
+class ConcurrentSchedule(Schedule):
+    def __init__(self, schedules: List[Schedule]):
+        self.__schedules = schedules
+
+    def config(self, val: List[Any], n: List[int], _min: List[float]):
+        for i in range(len(self.__schedules)):
+            self.__schedules[i].config(val[i], n[i], _min[i])
+
+    def step(self, count: Sequence, action: Sequence) -> NDArray[1, int]:
+        rewards: NDArray[1, int] = np.zeros(len(self.__schedules))
+        for i in range(len(self.__schedules)):
+            rew = self.__schedules[i].step(count[i], action[i])
+            rewards[i] = rew
+        return rewards
+
+    def finished(self) -> bool:
+        return sum(s.finished() for s in self.__schedules) > 0
+
+    def reset(self):
+        for i in range(len(self.__schedules)):
+            self.__schedules[i].reset()
+
+    @property
+    def val(self) -> List[Any]:
+        return [s.val for s in self.__schedules]
+
+    @property
+    def n(self) -> List[int]:
+        return [s.n for s in self.__schedules]
+
+    @property
+    def vals(self) -> List[Any]:
+        return [s.vals for s in self.__schedules]
