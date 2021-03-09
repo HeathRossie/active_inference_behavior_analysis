3d69ba4e3b3b89971a257a96f39d640afd03ba5e
7cm-diameter <from.this.ridiculous.loop@gmail.com>
2021-03-09 14:38:06 +0900

    make `Agent.__alpha` and `Agent.__beta` public to be overwrrote by a subclass

--- aibes/model.py
+++ aibes/model.py
@@ -49,28 +49,28 @@ class GAIStaticAgent(Agent):
     """
     def __init__(self, lamb: float, k: NumberOfOptions, lr: float):
         self.__alpha = np.exp(2 * lamb)
-        self.__alpha_t: NDArray[1, float] = np.ones(k)
-        self.__beta_t: NDArray[1, float] = np.ones(k)
+        self._alpha_t: NDArray[1, float] = np.ones(k)
+        self._beta_t: NDArray[1, float] = np.ones(k)
         self.__lr = lr
         self.__k = k
 
     def update(self, reward: NDArray[1, Reward], action: NDArray[1, Action]):
-        self.__alpha_t += reward * action * self.__lr
-        self.__beta_t += (1 - reward) * action * self.__lr
+        self._alpha_t += reward * action * self.__lr
+        self._beta_t += (1 - reward) * action * self.__lr
 
     def predict(self) -> Tuple[NDArray[1, Prediction], NDArray[1, float]]:
-        nu_t = self.__alpha_t + self.__beta_t
-        mu_t = self.__alpha_t / nu_t
-        kl_div_a = -betaln(self.__alpha_t, self.__beta_t) \
-            + (self.__alpha_t - self.__alpha) * digamma(self.__alpha_t) \
-            + (self.__beta_t - 1) * digamma(self.__beta_t) \
+        nu_t = self._alpha_t + self._beta_t
+        mu_t = self._alpha_t / nu_t
+        kl_div_a = -betaln(self._alpha_t, self._beta_t) \
+            + (self._alpha_t - self.__alpha) * digamma(self._alpha_t) \
+            + (self._beta_t - 1) * digamma(self._beta_t) \
             + (self.__alpha + 1 - nu_t) * digamma(nu_t)
-        h_a = - mu_t * digamma(self.__alpha_t + 1) \
-            - (1 - mu_t) * digamma(self.__beta_t + 1) \
+        h_a = - mu_t * digamma(self._alpha_t + 1) \
+            - (1 - mu_t) * digamma(self._beta_t + 1) \
             + digamma(nu_t + 1)
         epistemic = \
-            mu_t * (-np.log(mu_t) + digamma(self.__alpha_t + 1) - digamma(nu_t + 1)) \
-            + (1 - mu_t) * (-np.log(1 - mu_t) + digamma(self.__beta_t + 1) - digamma(nu_t + 1))
+            mu_t * (-np.log(mu_t) + digamma(self._alpha_t + 1) - digamma(nu_t + 1)) \
+            + (1 - mu_t) * (-np.log(1 - mu_t) + digamma(self._beta_t + 1) - digamma(nu_t + 1))
         pragmatic = kl_div_a + h_a - epistemic
         return pragmatic, epistemic
 
@@ -87,11 +87,11 @@ class GAIStaticAgent(Agent):
 
     @property
     def alpha_t(self) -> NDArray[1, float]:
-        return self.__alpha_t
+        return self._alpha_t
 
     @property
     def beta_t(self) -> NDArray[1, float]:
-        return self.__beta_t
+        return self._beta_t
 
     @property
     def k(self) -> int:
@@ -106,28 +106,28 @@ class SAIStaticAgent(Agent):
     """
     def __init__(self, lamb: float, k: NumberOfOptions, lr: float):
         self.__lambda = lamb
-        self.__alpha_t: NDArray[1, float] = np.ones(k)
-        self.__beta_t: NDArray[1, float] = np.ones(k)
+        self._alpha_t: NDArray[1, float] = np.ones(k)
+        self._beta_t: NDArray[1, float] = np.ones(k)
         self.__lr = lr
         self.__k = k
 
     def update(self, reward: Reward, action: Action):
-        self.__alpha_t += reward * action * self.__lr
-        self.__beta_t += (1 - reward) * action * self.__lr
+        self._alpha_t += reward * action * self.__lr
+        self._beta_t += (1 - reward) * action * self.__lr
 
     def predict(self) -> Tuple[NDArray[1, Prediction], NDArray[1, Prediction]]:
-        nu_t = self.__alpha_t + self.__beta_t
-        mu_t = self.__alpha_t / nu_t
+        nu_t = self._alpha_t + self._beta_t
+        mu_t = self._alpha_t / nu_t
 
         kl_div_a = - self.__lambda * (2 * mu_t - 1) \
             + mu_t * np.log(mu_t) \
             + (1 - mu_t) * np.log(1 - mu_t)
-        h_a = - mu_t * digamma(self.__alpha_t + 1) \
-            - (1 - mu_t) * digamma(self.__beta_t + 1) \
+        h_a = - mu_t * digamma(self._alpha_t + 1) \
+            - (1 - mu_t) * digamma(self._beta_t + 1) \
             + digamma(nu_t + 1)
         epistemic = \
-            mu_t * (-np.log(mu_t) + digamma(self.__alpha_t + 1) - digamma(nu_t + 1)) \
-            + (1 - mu_t) * (-np.log(1 - mu_t) + digamma(self.__beta_t + 1) - digamma(nu_t + 1))
+            mu_t * (-np.log(mu_t) + digamma(self._alpha_t + 1) - digamma(nu_t + 1)) \
+            + (1 - mu_t) * (-np.log(1 - mu_t) + digamma(self._beta_t + 1) - digamma(nu_t + 1))
         pragmatic = kl_div_a + h_a - epistemic
         return pragmatic, epistemic
 
@@ -144,11 +144,11 @@ class SAIStaticAgent(Agent):
 
     @property
     def alpha_t(self) -> NDArray[1, float]:
-        return self.__alpha_t
+        return self._alpha_t
 
     @property
     def beta_t(self) -> NDArray[1, float]:
-        return self.__beta_t
+        return self._beta_t
 
     @property
     def k(self) -> int:
@@ -158,10 +158,10 @@ class SAIStaticAgent(Agent):
 class GAIDynamicAgent(Agent):
     def __init__(self, lamb: float, k: NumberOfOptions, lr: float):
         self.__alpha = np.exp(2 * lamb)
-        self.__alpha_t: NDArray[1, float] = np.ones(k)
-        self.__alpha_zero = self.__alpha_t.copy()
-        self.__beta_t: NDArray[1, float] = np.ones(k)
-        self.__beta_zero = self.__beta_t.copy()
+        self._alpha_t: NDArray[1, float] = np.ones(k)
+        self.__alpha_zero = self._alpha_t.copy()
+        self._beta_t: NDArray[1, float] = np.ones(k)
+        self.__beta_zero = self._beta_t.copy()
         self.__a = np.full(k, 0.5)
         self.__b = np.full(k, 20.)
         self.__omega = np.zeros(k)
@@ -170,35 +170,35 @@ class GAIDynamicAgent(Agent):
 
     def update(self, reward: NDArray[1, Reward], action: NDArray[1, Action]):
         m = self.__a / (self.__a + self.__b)
-        mu = self.__alpha_t / (self.__alpha_t + self.__beta_t)
+        mu = self._alpha_t / (self._alpha_t + self._beta_t)
         eta = .5 * self.__omega / (.5 * self.__omega +
                                    (mu * reward + (1 - mu) *
                                     (1 - reward)) * (1 - self.__omega))
         self.__omega = m * (1 - eta)
-        alpha_t_new = (1 - eta) * self.__alpha_t \
+        alpha_t_new = (1 - eta) * self._alpha_t \
             + eta * self.__alpha_zero \
             + reward
-        self.__alpha_t += (alpha_t_new - self.__alpha_t) * action * self.__lr
-        beta_t_new = (1 - eta) * self.__beta_t \
+        self._alpha_t += (alpha_t_new - self._alpha_t) * action * self.__lr
+        beta_t_new = (1 - eta) * self._beta_t \
             + eta * self.__beta_zero \
             + (1 - reward)
-        self.__beta_t += (beta_t_new - self.__beta_t) * action * self.__lr
+        self._beta_t += (beta_t_new - self._beta_t) * action * self.__lr
         self.__a += self.__omega
         self.__b += 1 - eta - self.__omega
 
     def predict(self) -> Tuple[NDArray[1, Prediction], NDArray[1, Prediction]]:
-        nu_t = self.__alpha_t + self.__beta_t
-        mu_t = self.__alpha_t / nu_t
-        kl_div_a = -betaln(self.__alpha_t, self.__beta_t) \
-            + (self.__alpha_t - self.__alpha) * digamma(self.__alpha_t) \
-            + (self.__beta_t - 1) * digamma(self.__beta_t) \
+        nu_t = self._alpha_t + self._beta_t
+        mu_t = self._alpha_t / nu_t
+        kl_div_a = -betaln(self._alpha_t, self._beta_t) \
+            + (self._alpha_t - self.__alpha) * digamma(self._alpha_t) \
+            + (self._beta_t - 1) * digamma(self._beta_t) \
             + (self.__alpha + 1 - nu_t) * digamma(nu_t)
-        h_a = - mu_t * digamma(self.__alpha_t + 1) \
-            - (1 - mu_t) * digamma(self.__beta_t + 1) \
+        h_a = - mu_t * digamma(self._alpha_t + 1) \
+            - (1 - mu_t) * digamma(self._beta_t + 1) \
             + digamma(nu_t + 1)
         epistemic = \
-            mu_t * (-np.log(mu_t) + digamma(self.__alpha_t + 1) - digamma(nu_t + 1)) \
-            + (1 - mu_t) * (-np.log(1 - mu_t) + digamma(self.__beta_t + 1) - digamma(nu_t + 1))
+            mu_t * (-np.log(mu_t) + digamma(self._alpha_t + 1) - digamma(nu_t + 1)) \
+            + (1 - mu_t) * (-np.log(1 - mu_t) + digamma(self._beta_t + 1) - digamma(nu_t + 1))
         pragmatic = kl_div_a + h_a - epistemic
         return pragmatic, epistemic
 
@@ -215,11 +215,11 @@ class GAIDynamicAgent(Agent):
 
     @property
     def alpha_t(self) -> NDArray[1, float]:
-        return self.__alpha_t
+        return self._alpha_t
 
     @property
     def beta_t(self) -> NDArray[1, float]:
-        return self.__beta_t
+        return self._beta_t
 
     @property
     def k(self) -> int:
@@ -234,10 +234,10 @@ class SAIDynamicAgent(Agent):
     """
     def __init__(self, lamb: float, k: NumberOfOptions, lr: float):
         self.__lambda = lamb
-        self.__alpha_t: NDArray[1, float] = np.ones(k)
-        self.__alpha_zero = self.__alpha_t.copy()
-        self.__beta_t: NDArray[1, float] = np.ones(k)
-        self.__beta_zero = self.__beta_t.copy()
+        self._alpha_t: NDArray[1, float] = np.ones(k)
+        self.__alpha_zero = self._alpha_t.copy()
+        self._beta_t: NDArray[1, float] = np.ones(k)
+        self.__beta_zero = self._beta_t.copy()
         self.__a = np.full(k, 0.5)
         self.__b = np.full(k, 20.)
         self.__omega = np.zeros(k)
@@ -246,35 +246,35 @@ class SAIDynamicAgent(Agent):
 
     def update(self, reward: NDArray[1, Reward], action: NDArray[1, Action]):
         m = self.__a / (self.__a + self.__b)
-        mu = self.__alpha_t / (self.__alpha_t + self.__beta_t)
+        mu = self._alpha_t / (self._alpha_t + self._beta_t)
         eta = .5 * self.__omega / (.5 * self.__omega +
                                    (mu * reward + (1 - mu) *
                                     (1 - reward)) * (1 - self.__omega))
         self.__omega = m * (1 - eta)
-        alpha_t_new = (1 - eta) * self.__alpha_t \
+        alpha_t_new = (1 - eta) * self._alpha_t \
             + eta * self.__alpha_zero \
             + reward
-        self.__alpha_t += (alpha_t_new - self.__alpha_t) * action * self.__lr
-        beta_t_new = (1 - eta) * self.__beta_t \
+        self._alpha_t += (alpha_t_new - self._alpha_t) * action * self.__lr
+        beta_t_new = (1 - eta) * self._beta_t \
             + eta * self.__beta_zero \
             + (1 - reward)
-        self.__beta_t += (beta_t_new - self.__beta_t) * action * self.__lr
+        self._beta_t += (beta_t_new - self._beta_t) * action * self.__lr
         self.__a += self.__omega
         self.__b += 1 - eta - self.__omega
 
     def predict(self) -> Tuple[NDArray[1, Prediction], NDArray[1, Prediction]]:
-        nu_t = self.__alpha_t + self.__beta_t
-        mu_t = self.__alpha_t / nu_t
+        nu_t = self._alpha_t + self._beta_t
+        mu_t = self._alpha_t / nu_t
 
         kl_div_a = - self.__lambda * (2 * mu_t - 1) \
             + mu_t * np.log(mu_t) \
             + (1 - mu_t) * np.log(1 - mu_t)
-        h_a = - mu_t * digamma(self.__alpha_t + 1) \
-            - (1 - mu_t) * digamma(self.__beta_t + 1) \
+        h_a = - mu_t * digamma(self._alpha_t + 1) \
+            - (1 - mu_t) * digamma(self._beta_t + 1) \
             + digamma(nu_t + 1)
         epistemic = \
-            mu_t * (-np.log(mu_t) + digamma(self.__alpha_t + 1) - digamma(nu_t + 1)) \
-            + (1 - mu_t) * (-np.log(1 - mu_t) + digamma(self.__beta_t + 1) - digamma(nu_t + 1))
+            mu_t * (-np.log(mu_t) + digamma(self._alpha_t + 1) - digamma(nu_t + 1)) \
+            + (1 - mu_t) * (-np.log(1 - mu_t) + digamma(self._beta_t + 1) - digamma(nu_t + 1))
         pragmatic = kl_div_a + h_a - epistemic
         return pragmatic, epistemic
 
@@ -291,11 +291,11 @@ class SAIDynamicAgent(Agent):
 
     @property
     def alpha_t(self) -> NDArray[1, float]:
-        return self.__alpha_t
+        return self._alpha_t
 
     @property
     def beta_t(self) -> NDArray[1, float]:
-        return self.__beta_t
+        return self._beta_t
 
     @property
     def k(self) -> int:
